8faf3111c89347dc46487d014fd6581f43ccaf3f
boolick <boolick38@gmail.com>
2024-01-22 22:12:43 +0300

    feat: add feature to update folder 'files-copy': to remove subfolders before copy!

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,6 +8,7 @@ async function copyDir() {
   try {
     await fs.mkdir(destinationFolderPath, { recursive: true });
     const files = await fs.readdir(sourceFolderPath);
+    await cleanDestinationFolder(files);
 
     for (const file of files) {
       const sourceFilePath = path.join(sourceFolderPath, file);
@@ -28,15 +29,51 @@ async function copyDir() {
     console.error('Error copying directory:', err.message);
   }
 }
+
+async function cleanDestinationFolder(sourceFiles) {
+  // Читаем содержимое files-copy
+  const destinationFiles = await fs.readdir(destinationFolderPath);
+
+  const itemsToRemove = destinationFiles.filter(
+    (item) => !sourceFiles.includes(item),
+  );
+
+  for (const itemToRemove of itemsToRemove) {
+    const itemPathToRemove = path.join(destinationFolderPath, itemToRemove);
+    await removeItemRecursive(itemPathToRemove);
+  }
+}
+
+async function removeItemRecursive(itemPath) {
+  const itemStats = await fs.stat(itemPath);
+
+  if (itemStats.isFile()) {
+    // Если это файл, удаляем его
+    await fs.unlink(itemPath);
+  } else if (itemStats.isDirectory()) {
+    // Если это директория, рекурсивно удаляем
+    const items = await fs.readdir(itemPath);
+
+    for (const item of items) {
+      const itemFullPath = path.join(itemPath, item);
+      await removeItemRecursive(itemFullPath);
+    }
+
+    // Удаляем саму папку
+    await fs.rmdir(itemPath);
+  }
+}
+
 async function copyDirRecursive(source, destination) {
+  // Копируем файлы и папки
   await fs.mkdir(destination, { recursive: true });
 
   const files = await fs.readdir(source);
 
+  // рекурсивно копируем файлы
   for (const file of files) {
     const sourceFilePath = path.join(source, file);
     const destinationFilePath = path.join(destination, file);
-
     const fileStats = await fs.stat(sourceFilePath);
 
     if (fileStats.isFile()) {
